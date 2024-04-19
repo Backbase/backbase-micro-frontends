0d64b15bdd1b2754a71dc57eda1faa67894dda97
Pablo Villoslada Puigcerber <pablov@backbase.com>
2024-04-18 20:22:28 -0600

    fix: navigation within the remote Angular 12 app
    First time we try to navigate within the remote app, e.g. from accounts
    to account statements, the component doesn't render.
    
    Interestingly, it seems like some kind of race condition because once
    loaded, subsequent navigation renders the component without issues as
    the lazy loaded module bundle is already downloaded.
    
    A custom web component wrapper with a setTimeout before creating the
    element seems to help, but it's unreliable.
    
    So we have gone with a custom event as a way to communicate both apps,
    thus leaving remote navigation to be handled entirely by the remote app.

--- legacy-as-remote/angular12/src/app/app.component.ts
+++ legacy-as-remote/angular12/src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { connectRouter } from '@angular-architects/module-federation-tools';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  eventName = 'angular12:navigate';
   title = 'angular12';
 
-  constructor(private router: Router) {
+  constructor(private readonly router: Router) {
     connectRouter(this.router);
+    window.addEventListener(this.eventName, this.navigationHandler as (e: Event) => void, false);
   }
+
+  ngOnDestroy(): void {
+    window.removeEventListener(this.eventName, this.navigationHandler as (e: Event) => void, false);
+  }
+
+  private navigationHandler = (event: CustomEvent): void => {
+    if (event.detail.url) {
+      this.router.navigateByUrl(event.detail.url);
+    }
+  };
 }
